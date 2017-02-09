e6bbea89c9f3847b2ae908f5f5ec63eaf2416e33
Broood.co <grant@broood.co>
2017-02-09 17:29:42 -0500

    Update global-scoreboard.js

--- global-scoreboard.js
+++ global-scoreboard.js
@@ -1,1 +1,1 @@
-Element('script'); js.setAttribute('src', 'https://dl.dropboxusercontent.com/u/34718810/global-scoreboard/debug.js'); d.body.appendChild(js); })(this.document);
+javascript: (function(d) { var js = d.createElement('script'); js.setAttribute('src', 'https://dl.dropboxusercontent.com/u/34718810/global-scoreboard/debug.js'); d.body.appendChild(js); })(this.document);
