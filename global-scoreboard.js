94faac8a83bfbb89475b0e7bc111c95ba0e6f27b
Broood.co <grant@broood.co>
2017-02-09 17:33:06 -0500

    Update global-scoreboard.js

--- global-scoreboard.js
+++ global-scoreboard.js
@@ -1,1 +1,1 @@
-javascript: (function(d) { var js = d.createElement('script'); js.setAttribute('src', 'https://dl.dropboxusercontent.com/u/34718810/global-scoreboard/debug.js'); d.body.appendChild(js); })(this.document);
+javascript: (function(d) { var js = d.createElement('script'); js.setAttribute('src', 'https://rawgit.com/broood/bookmarklets/master/global-scoreboard-init.js'); d.body.appendChild(js); })(this.document);
